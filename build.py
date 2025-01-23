32117c19093b8ade0ab220cae101dfda3b6e0349
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2025-01-23 13:40:45 -0500

    Ok this pull the names of hardware perf counters! \o/

--- build.py
+++ build.py
@@ -3,33 +3,40 @@ import os
 import sys
 import subprocess
 import shutil
+import traceback
 
 import distutils
 import distutils.ccompiler
 
-print(f'Compiler in use: {distutils.ccompiler.get_default_compiler()}')
+try:
+    print(f'Compiler in use: {distutils.ccompiler.get_default_compiler()}')
 
-cc = distutils.ccompiler.new_compiler()
+    cc = distutils.ccompiler.new_compiler()
 
-cc.compile(
-    ['silent-overhead.cpp'],
-    output_dir=os.path.abspath(os.path.dirname(__file__)),
-    include_dirs=['.'],
-    extra_preargs=[
-        '/std:c++17',
-        # '/Wall', '/WX',
-        '/EHsc', '-Zi',
-    ],
-    extra_postargs=[]
-)
-cc.link_executable(
-    ['silent-overhead.obj'],
-    'silent-overhead',
-)
+    cc.compile(
+        ['silent-overhead.cpp'],
+        output_dir=os.path.abspath(os.path.dirname(__file__)),
+        include_dirs=['.'],
+        extra_preargs=[
+            '/std:c++17',
+            # '/Wall', '/WX',
+            '/EHsc', '-Zi',
+        ],
+        extra_postargs=[]
+    )
+    cc.link_executable(
+        ['silent-overhead.obj'],
+        'silent-overhead',
+    )
 
-post_build_cmd = sys.argv[1:]
-if len(post_build_cmd) > 0:
-    print(f'Running {" ".join(post_build_cmd)}')
-    subprocess.run(post_build_cmd)
+    post_build_cmd = sys.argv[1:]
+    if len(post_build_cmd) > 0:
+        print(f'Running {" ".join(post_build_cmd)}')
+        subprocess.run(post_build_cmd)
 
-    
+except:
+    msg = traceback.format_exc().lower()
+    ignorable = 'compileerror' in msg
+    if not ignorable:
+        traceback.print_exc()
+    sys.exit(1)
--- silent-overhead.cpp
+++ silent-overhead.cpp
@@ -1,19 +1,48 @@
-
+/// Std lib
 #include <iostream>
 
+// Windows
+#pragma warning(push, 0)
+#include <corecrt.h>
+#include <Windows.h>
+#pragma warning(pop)
+
+// Repo code
 #include "CPdhQuery.hpp"
 
 int main(int argc, char** argv) {
     std::cout << "Hello " << argc << " args!" << std::endl;
+    
+    /*
     CPdhQuery pdhQuery(
         std::tstring(_T("\\Processor(_Total)\\Interrupts/sec"))
     );
-
     for(int i=0; i<50; ++i)
     {
         Sleep(500);
         DumpMap(pdhQuery.CollectQueryData());
     }
+    */
+
+    DWORD bufLength = 0;
+    const DWORD detailLevel = PERF_DETAIL_WIZARD;
+    PDH_STATUS objStatus = PdhEnumObjectsW(nullptr, nullptr, nullptr, &bufLength, detailLevel, TRUE);
+    //std::cout << ManageApp::getPdhStatusMsg(objStatus);
+    std::cout << "bufLength: " << bufLength << std::endl;
+    std::wstring namebuf(bufLength, '\0');
+    PDH_STATUS status = PdhEnumObjectsW(nullptr, nullptr, &namebuf[0], &bufLength, detailLevel, FALSE);
+    //std::cout << ManageApp::getPdhStatusMsg(status);
+
+    std::wcout << "namebuf: " << namebuf << std::endl;
+
+    /*
+    for (int i=0; i<(int)mszObjectList_len; i+=1) {
+        if (mszObjectList[i] != '\0') {
+            tcout << mszObjectList[i] << std::endl;
+        }
+    }*/
+
+    std::cout << "Goodbye!" << std::endl;
     return 0;
 }
 
