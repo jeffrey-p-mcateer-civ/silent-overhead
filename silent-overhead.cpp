f7d17fd6ca2667b217ed3d09f4ed296513773eb1
Jeffrey McAteer <jeffrey.p.mcateer.civ@mail.mil>
2025-01-23 13:02:22 -0500

    Boilerplate

--- silent-overhead.cpp
+++ silent-overhead.cpp
@@ -1,9 +1,19 @@
 
 #include <iostream>
 
+#include "CPdhQuery.hpp"
+
 int main(int argc, char** argv) {
     std::cout << "Hello " << argc << " args!" << std::endl;
-    
+    CPdhQuery pdhQuery(
+        std::tstring(_T("\\Processor(_Total)\\Interrupts/sec"))
+    );
+
+    for(int i=0; i<50; ++i)
+    {
+        Sleep(500);
+        DumpMap(pdhQuery.CollectQueryData());
+    }
     return 0;
 }
 
